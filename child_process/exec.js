9ae191fdafc1f821c08a84f1b2ff2526d8dd1a1c
sofia gomez andurno <sofigomezandurno@gmail.com>
2022-12-24 16:13:21 -0300

    add changes

--- child_process/exec.js
+++
@@ -1,7 +0,0 @@
-const { exec } = require('child_process');
-
-exec('ls -lh', (error, stdout, stderr) => {
-    if (error) return console.error(`Error --EXEC--:\n${error.message}`);
-    if (stderr) return console.error(`Error --> STDERR:\n${error}`);
-    console.error(`--STDOUT--:\n${stdout}`);
-});
--- child_process/execFile.js
+++
@@ -1,7 +0,0 @@
-const { execFile } = require('child_process');
-
-execFile(__dirname + '/processNodejsImage.sh', (error, stdout, stderr) => {
-    if (error) return console.log(`Error --EXECFILE--:\n${error.message}`);
-    if (stderr) return console.log(`Error --> STDERR:\n${stderr}`);
-    console.log(`--STDOUT--:\n${stdout}`);
-});
